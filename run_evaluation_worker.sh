d0004bd99f16d41dd455e9a07fddd6d2cc1e3d3d
Guillaume BERTHELOT <gberthelot@numalis.com>
2024-07-22 17:02:57 +0200

    wip

--- run_evaluation_worker.sh
+++ run_evaluation_worker.sh
@@ -5,21 +5,19 @@ export PYTHONPATH=$PYTHONPATH:"./AbstractRay"
 NETWORK_FILE="./AbstratRay/src/CNN/simple_cnn_fashionmnist.pth"
 INPUT_IMAGE="./cut_dog.jpeg"
 NETWORK_NAME="vgg16"
-NUM_WORKER=1
 BACK_END="cpu"
-RAM=
+RAM=64 
 RESIZE_INPUT="True"
-RESIZE_WIDTH=224
-RESISE_HEIGHT=224
+RESIZE_WIDTH=112    
+RESISE_HEIGHT=112
 BOX_INPUT="False"
 ADD_SYMBOL="True"
 RELEVANCE_DUMP="False"
-#LAST_LAYER=4
-
+LAST_LAYER=4
 
 NOISE=0.00001
 
-while (( $(echo "$NUM_WORKER >= 10" | bc -l) )); do
+for NUM_WORKER in {1..10}; do
     python ./AbstractRay/main.py \
         --network_file $NETWORK_FILE \
         --input_image $INPUT_IMAGE \
@@ -34,7 +32,5 @@ while (( $(echo "$NUM_WORKER >= 10" | bc -l) )); do
         --box_input $BOX_INPUT \
         --add_symbol $ADD_SYMBOL \
         --relevance_dump $RELEVANCE_DUMP\
-        #--model_last_layer $LAST_LAYER
-     NUM_WORKER=$(echo "$NUM_WORKER + 1" | bc)
-
+        --model_last_layer $LAST_LAYER
 done
